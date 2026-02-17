36ace98b40f95501c6bac8c72f3298246e4c4c40
fjt-dev <157100279+fjt-dev@users.noreply.github.com>
2026-02-17 23:52:29 +0900

    add: social-links component

---
+++ components/social-links.tsx
@@ -0,0 +1,9 @@
+export default function SocialLinks () {
+  return (
+    <div>
+      <a href="https://github.com/fjt-dev">GitHub</a>
+      <a href="https://twitter.com/iQV5K">Twitter</a>
+      <a href="https://prismisskey.space/@fjtd">Misskey</a>
+    </div>
+  )
+}
