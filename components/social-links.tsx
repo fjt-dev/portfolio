fd1687c63e9f929e3d580a494b7574ed9648255e
fjt-dev <157100279+fjt-dev@users.noreply.github.com>
2026-02-19 01:06:55 +0900

    add: logo

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,17 +1,30 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
+import { Github, Twitter } from 'lucide-react';
+import { siMisskey } from 'simple-icons';
 
-export default function SocialLinks () {
+export default function SocialLinks() {
   return (
-    <div>
+    <div className='flex gap-2'>
       <Button variant="outline" asChild>
-        <a href="https://github.com/fjt-dev">GitHub</a>
+        <a href="https://github.com/fjt-dev">
+          <Github className="w-5 h-5" />
+          GitHub
+        </a>
       </Button>
       <Button variant="outline" asChild>
-        <a href="https://twitter.com/iQV5K">Twitter</a>
+        <a href="https://twitter.com/iQV5K">
+          <Twitter className="w-5 h-5" />
+          Twitter
+        </a>
       </Button>
       <Button variant="outline" asChild>
-        <a href="https://prismisskey.space/@fjtd">Misskey</a>
+        <a href="https://prismisskey.space/@fjtd">
+          <svg viewBox="0 0 24 24" role="img" className="w-5 h-5">
+            <path d={siMisskey.path} />
+          </svg>
+          Misskey
+        </a>
       </Button>
     </div>
-  )
+  );
 }
