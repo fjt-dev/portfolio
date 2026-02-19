445db9357cd8d177b4055061ce82acf2f47ad152
fjt-dev <157100279+fjt-dev@users.noreply.github.com>
2026-02-19 22:46:12 +0900

    improve layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import SocialLinks from "@/components/social-links"
-
+import SocialLinks from '@/components/social-links';
 
 export default function Home() {
   return (
-    <div>
-      <SocialLinks />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center space-y-8">
+        <div>
+          <h1 className="text-6xl font-bold mb-2">fjtd.dev</h1>
+          <p className="text-lg text-muted-foreground">Student | Aspiring Web Frontend Engineer</p>
+        </div>
+        <div className="border rounded-lg p-8">
+          {/* <p className="">sample</p> */}
+          <SocialLinks />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
