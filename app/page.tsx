92a2325a5843a9d48b9036821e9c4caaf7991ce4
fjt-dev <157100279+fjt-dev@users.noreply.github.com>
2026-02-21 01:10:47 +0900

    seo

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <h1 className="text-5xl sm:text-6xl font-bold mb-2">fjtd.dev</h1>
           <p className="text-lg text-muted-foreground">Student | Aspiring Web Frontend Engineer</p>
         </div>
-        <div className="border rounded-lg p-6 sm:p-8">
+        <div className="border rounded-lg p-6 sm:p-8 inline-block min-w-0">
           {/* <p className="">sample</p> */}
           <SocialLinks />
         </div>
--- components/social-links.tsx
+++ components/social-links.tsx
@@ -4,22 +4,22 @@ import { siMisskey } from 'simple-icons';
 
 export default function SocialLinks() {
   return (
-    <div className='flex flex-col sm:flex-row gap-2'>
-      <Button variant="outline" asChild>
+    <div className='flex flex-col sm:flex-row gap-3 justify-center items-center'>
+      <Button variant="outline" asChild className='w-full sm:w-fit min-w-0'>
         <a href="https://github.com/fjt-dev">
           <Github className="w-5 h-5" />
           GitHub
         </a>
       </Button>
-      <Button variant="outline" asChild>
+      <Button variant="outline" asChild className='w-full sm:w-fit min-w-0'>
         <a href="https://twitter.com/iQV5K">
           <Twitter className="w-5 h-5" />
           Twitter
         </a>
       </Button>
-      <Button variant="outline" asChild>
+      <Button variant="outline" asChild className='w-full sm:w-fit min-w-0'>
         <a href="https://prismisskey.space/@fjtd">
-          <svg viewBox="0 0 24 24" role="img" className="w-5 h-5">
+          <svg viewBox="0 0 24 24" role="img" className="w-5 h-5 shrink-0">
             <path d={siMisskey.path} />
           </svg>
           Misskey
